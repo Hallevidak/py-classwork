25bcaa4b3bb68badf41523ad2c407cf6051ab329
Halle Vidak <vidakh@queens.edu>
2026-01-29 10:01:38 -0500

    week 2 class 2

---
+++ # Variables.py
@@ -0,0 +1,44 @@
+# Variables
+
+
+
+
+
+
+
+# string 
+string = "any sequenc of characters" # strings use quotes or '"
+
+# never name vairables this way 
+s  str("something else")
+
+#snake casing, not hyphens, not NumberString, numberString
+number_string = ""
+print(type(string))
+print(types), s)
+
+#Jupyter will print (display) the last line. 
+number_string
+
+# int 
+x = 1
+y = int('3')
+print(integer)
+print(type(y))
+
+print("x + y =", x + y)
+#CONCATINATION IS DIFFERENT FROM ADDDITION
+STRING = STRING(X))
+
+#AlIGNING INFO TO OUTPUT 
+PRINT(F"X PLUS Y EQUALS {X + Y}, BUT WITH AN F STRING")
+
+# Boolean = true/false, 1/0
+
+is_true = True #or False
+is_false = bool(0)
+print(is_true)
+print(is_false)
+
+
+
